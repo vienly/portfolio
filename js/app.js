b7ff4541bbc1aad2d1daf6dd1e48abf97ff62838
Gin <vienlyt@gmail.com>
2016-05-19 14:28:44 -0700

    some clean up and set up columns for about section

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   var $menu = $('#menu'), $menulink = $('.menu-link'), $wrapper = $('#wrapper');
+  $menulink.find('button').text('>>');
 
   $menulink.on('click', function() {
-    console.log('active');
     $menulink.toggleClass('active');
     $wrapper.toggleClass('active');
+
+    if ($menulink.hasClass('active')) {
+      $menulink.find('button').text('<<');
+    }
+    else
+      $menulink.find('button').text('>>');
+
     return false;
   });
 });
